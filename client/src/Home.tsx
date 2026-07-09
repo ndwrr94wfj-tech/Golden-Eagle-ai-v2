1c8188aaf7e417893bbce271f554d87b0750a1b7
azerkash[777] <ndwrr94wfj@privaterelay.appleid.com>
2026-07-09 05:39:39 +0200

    Add Home component with welcome message

---
+++ client/src/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+      <h1>Golden Eagle AI v2 is Ready!</h1>
+    </div>
+  );
+}
